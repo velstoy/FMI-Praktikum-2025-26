b0cb8666b98c145cbdfcc7cba8d3a6c54378e75f
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2025-10-14 23:19:26 +0300

    added new lines

--- Week1/Task4/Task4.cpp
+++ Week1/Task4/Task4.cpp
@@ -5,9 +5,9 @@ int main()
 	int num1, num2;
 	std::cin >> num1 >> num2;
 
-	std::cout << "Sum: " << num1 + num2;
-	std::cout << "Difference: " << num1 - num2;
-	std::cout << "Product: " << num1 * num2;
+	std::cout << "Sum: " << num1 + num2 << std::endl;
+	std::cout << "Difference: " << num1 - num2 << std::endl;
+	std::cout << "Product: " << num1 * num2 << std::endl;
 	std::cout << "Quotient: " << num1 / num2;
 	return 0;
 }
