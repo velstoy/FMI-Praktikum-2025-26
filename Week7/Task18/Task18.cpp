8ed1dfff45d9b58469f119908b17b529fe8b8f81
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2025-11-21 16:07:42 +0200

    done 18

--- Week7/Task18/Task18.cpp
+++ Week7/Task18/Task18.cpp
@@ -1,20 +1,47 @@
-// Task18.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
 
-#include <iostream>
+constexpr size_t N = 50;
+
+void inputMatrix(int matrix[][N], size_t size);
+bool isMatrixTriangular(const int matrix[][N], size_t size);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int matrix[N][N];
+
+	int n;
+	std::cin >> n;
+
+	inputMatrix(matrix, n);
+
+	std::cout << std::boolalpha << isMatrixTriangular(matrix, n);
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void inputMatrix(int matrix[][N], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			std::cin >> matrix[i][j];
+		}
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool isMatrixTriangular(const int matrix[][N], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			if (i > j && matrix[i][j] != 0)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
