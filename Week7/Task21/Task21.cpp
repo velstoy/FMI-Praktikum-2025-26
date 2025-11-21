ceaa9a57c2dee2178415fb41e6c67f4c37e6f7b6
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2025-11-21 17:33:12 +0200

    done 21

--- Week7/Task21/Task21.cpp
+++ Week7/Task21/Task21.cpp
@@ -1,20 +1,106 @@
-// Task21.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
 
-#include <iostream>
+constexpr size_t N = 50;
+
+void inputMatrix(int matrix[][N], size_t size);
+void printMatrix(int matrix[][N], size_t size);
+void addMatrices(const int matrix1[][N], const int matrix2[][N], size_t size, int result[][N]);
+void multiplyMatrixWithNumber(const int matrix[][N], size_t size, int number, int result[][N]);
+void multiplyMatrices(const int matrix1[][N], const int matrix2[][N], size_t size, int result[][N]);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int matrix1[N][N], matrix2[N][N];
+
+	int n;
+	std::cin >> n;
+
+	inputMatrix(matrix1, n);
+	inputMatrix(matrix2, n);
+
+	int addition[N][N];
+	addMatrices(matrix1, matrix2, n, addition);
+
+	std::cout << "Addition: " << std::endl;
+	printMatrix(addition, n);
+
+	int numberMultiplication[N][N];
+
+	multiplyMatrixWithNumber(matrix1, n, 5, numberMultiplication);
+
+	std::cout << "Multiplication with number: " << std::endl;
+	printMatrix(numberMultiplication, n);
+
+	int multiplication[N][N];
+
+	multiplyMatrices(matrix1, matrix2, n, multiplication);
+
+	std::cout << "Matrix multiplication: " << std::endl;
+	printMatrix(multiplication, n);
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void inputMatrix(int matrix[][N], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			std::cin >> matrix[i][j];
+		}
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void printMatrix(int matrix[][N], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+}
+
+void addMatrices(const int matrix1[][N], const int matrix2[][N], size_t size, int result[][N])
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			result[i][j] = matrix1[i][j] + matrix2[i][j];
+		}
+	}
+}
+
+void multiplyMatrixWithNumber(const int matrix[][N], size_t size, int number, int result[][N])
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			result[i][j] = matrix[i][j] * number;
+		}
+	}
+}
+
+void multiplyMatrices(const int matrix1[][N], const int matrix2[][N], size_t size, int result[][N])
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			int sum = 0;
+			for (size_t k = 0; k < size; k++)
+			{
+				sum += matrix1[i][k] * matrix2[k][j];
+			}
+
+			result[i][j] = sum;
+		}
+	}
+}
